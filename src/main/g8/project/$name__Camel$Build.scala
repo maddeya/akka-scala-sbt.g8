35acee922de8c38c2d8dca126370bbd43fde8346
Reto Matter <reto.matter@gmail.com>
2013-06-28 17:42:28 +0200

    fixed missing comma

--- src/main/g8/project/$name__Camel$Build.scala
+++ src/main/g8/project/$name__Camel$Build.scala
@@ -13,14 +13,14 @@ object $name;format="Camel"$Build extends Build {
       scalaVersion := "2.10.2",
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
       libraryDependencies ++= Seq(
-        "com.typesafe.akka" %% "akka-actor" % "$akka_version$"
-        "com.typesafe.akka" %% "akka-testkit" % "2.1.4",
-        "com.typesafe.akka" %% "akka-kernel" % "2.1.4",
-        "com.typesafe.akka" %% "akka-agent" % "2.1.4",
-        "com.typesafe.akka" %% "akka-transactor" % "2.1.4",
+        "com.typesafe.akka" %% "akka-actor" % "$akka_version$",
+        "com.typesafe.akka" %% "akka-testkit" % "$akka_version$",
+        "com.typesafe.akka" %% "akka-kernel" % "$akka_version$",
+        "com.typesafe.akka" %% "akka-agent" % "$akka_version$",
+        "com.typesafe.akka" %% "akka-transactor" % "$akka_version$",
         "org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test",
         "junit" % "junit" % "4.11" % "test"
-      )
+      ),
       scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
     )
   )
