90d1c4dcd132990f94c5c74ef258b77517e701f0
Reto Matter <reto.matter@gmail.com>
2013-06-28 17:32:46 +0200

    changed akka version 2.1.4

--- src/main/g8/project/$name__Camel$Build.scala
+++ src/main/g8/project/$name__Camel$Build.scala
@@ -10,9 +10,18 @@ object $name;format="Camel"$Build extends Build {
       name := "$name$",
       organization := "$organization$",
       version := "$version$",
-      scalaVersion := "2.9.2",
+      scalaVersion := "2.10.2",
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
-      libraryDependencies += "com.typesafe.akka" % "akka-actor" % "$akka_version$"
+      libraryDependencies ++= Seq(
+        "com.typesafe.akka" %% "akka-actor" % "$akka_version$"
+        "com.typesafe.akka" %% "akka-testkit" % "2.1.4",
+        "com.typesafe.akka" %% "akka-kernel" % "2.1.4",
+        "com.typesafe.akka" %% "akka-agent" % "2.1.4",
+        "com.typesafe.akka" %% "akka-transactor" % "2.1.4",
+        "org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test",
+        "junit" % "junit" % "4.11" % "test"
+      )
+      scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
     )
   )
 }
